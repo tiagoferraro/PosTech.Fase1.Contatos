03216476d978ed8a235b58a5d78c21a8c2b58d72
Tiago Ferraro Augusto <tiagoferraro@gmail.com>
2024-06-26 20:57:03 -0300

    Criação da classe de IOC e associação no program

---
+++ PosTech.Fase1.Contatos.IoC/DependecyInjection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PosTech.Fase1.Contatos.IoC
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AdicionarDependencias(this IServiceCollection services)
+        {
+            
+            return services;
+        }
+    }
+}
