499624f71fa0857bc1b57753e715edf234d4b94d
Luís Fernando Fernandes <luisfernando.fernandes@hotmail.com>
2024-07-20 19:33:51 -0300

    fix: correção nas regras do validator de DDD para mostrar sempre a mensagem definida quando a regra não for cumprida.

--- PosTech.Fase1.Contatos.Application/Validators/DDDValidator.cs
+++ PosTech.Fase1.Contatos.Application/Validators/DDDValidator.cs
@@ -1,6 +1,5 @@
 ﻿using FluentValidation;
 using PosTech.Fase1.Contatos.Application.DTO;
-using PosTech.Fase1.Contatos.Domain.Entities;
 
 namespace PosTech.Fase1.Contatos.Application.Validators;
 
@@ -8,11 +7,14 @@ public class DDDValidator : AbstractValidator<DDDDto>
 {
     public DDDValidator()
     {
-        RuleFor(x => x.UfSigla).NotEmpty().Length(2).WithMessage("UfSigla precisa ser informada e conter extamente 2 caracteres ex:SP") 
-                            //    .Must(x => UnidadeFederativa.ufs.Contains(x))  // está dando erro pois nao reconhece a classe estática UnidadeFederativa
-                               ; // validar UF informada 
 
-        RuleFor(x => x.Regiao).NotEmpty().MaximumLength(100).WithMessage("UfRegiao é a principal cidade da área de abrangencia, precisa ser informada, e conter no máximo 100 caracteres") ;
+        RuleFor(x => x.UfSigla)
+            .Must(ufSigla => !string.IsNullOrEmpty(ufSigla) && ufSigla.Length == 2)
+            .WithMessage("UfSigla precisa ser informada e conter exatamente 2 caracteres ex:SP");
+
+        RuleFor(x => x.Regiao)
+            .Must(regiao => !string.IsNullOrEmpty(regiao) && regiao.Length <= 100)
+            .WithMessage("UfRegiao é a principal cidade da área de abrangência, precisa ser informada, e conter no máximo 100 caracteres");
 
     }
 }
