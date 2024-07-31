a5b72a26d84fb3adb2c457ddef01ff3d64308b53
Luís Fernando Fernandes <luisfernando.fernandes@hotmail.com>
2024-07-31 19:56:24 -0300

    refact: melhorias no codigo do teste do ddd validator

--- PosTech.Fase1.Contatos.Tests/Application/DDDValidatorTest.cs
+++ PosTech.Fase1.Contatos.Tests/Application/DDDValidatorTest.cs
@@ -5,157 +5,71 @@ namespace PosTech.Fase1.Contatos.Tests.Application
 {
     public class DDDValidatorTest
     {
-        [Fact]
-        public void DDDValidator_UfSiglaInformada_DeveRetornarSucesso()
+        private readonly DDDValidator _dddValidator;
+
+        public DDDValidatorTest()
         {
-            //arrange
-            var dddDto = new DDDDto()
+            _dddValidator = new DDDValidator();
+        }
+
+        [Theory]
+        [InlineData("SP", true, null)]
+        [InlineData("", false, "UfSigla precisa ser informada e conter exatamente 2 caracteres ex:SP")]
+        [InlineData("S", false, "UfSigla precisa ser informada e conter exatamente 2 caracteres ex:SP")]
+        [InlineData("SP1", false, "UfSigla precisa ser informada e conter exatamente 2 caracteres ex:SP")]
+        public void DDDValidator_UfSigla_DeveRetornarResultadoEsperado(string ufSigla, bool expectedIsValid, string expectedErrorMessage)
+        {
+            // Arrange
+            var dddDto = new DDDDto
             {
                 DddId = 11,
                 Regiao = "São Paulo",
+                UfNome = "São Paulo",
+                UfSigla = ufSigla
+            };
+
+            // Act
+            var result = _dddValidator.Validate(dddDto);
+
+            // Assert
+            Assert.Equal(expectedIsValid, result.IsValid);
+            if (!expectedIsValid)
+            {
+                Assert.Equal(expectedErrorMessage, result.Errors[0].ErrorMessage);
+            }
+        }
+
+        [Theory]
+        [InlineData("São Paulo", true, null)]
+        [InlineData("", false, "UfRegiao é a principal cidade da área de abrangência, precisa ser informada, e conter no máximo 100 caracteres")]
+        [InlineData("São Paulo, Campinas, Jundiaí, São José dos Campos, Guarulhos, Osasco, Santo André, São Bernardo do Campo", false, "UfRegiao é a principal cidade da área de abrangência, precisa ser informada, e conter no máximo 100 caracteres")]
+        public void DDDValidator_UfRegiao_DeveRetornarResultadoEsperado(string regiao, bool expectedIsValid, string expectedErrorMessage)
+        {
+            // Arrange
+            var dddDto = new DDDDto
+            {
+                DddId = 11,
+                Regiao = regiao,
                 UfNome = "São Paulo",
                 UfSigla = "SP"
             };
 
-            //act
-            var dddValidator = new DDDValidator();
-            var result = dddValidator.Validate(dddDto);
+            // Act
+            var result = _dddValidator.Validate(dddDto);
 
-            //assert
-            Assert.True(result.IsValid);
-        }
-   
-
-        [Fact]
-        public void DDDValidator_UfSiglaNaoInformada_DeveRetornarErro()
-        {
-            //arrange
-            var dddDto = new DDDDto()
+            // Assert
+            Assert.Equal(expectedIsValid, result.IsValid);
+            if (!expectedIsValid)
             {
-                DddId = 11,
-                Regiao = "São Paulo",
-                UfNome = "São Paulo",
-                UfSigla = ""
-            };
-
-            //act
-            var dddValidator = new DDDValidator();
-            var result = dddValidator.Validate(dddDto);
-
-            //assert
-            Assert.False(result.IsValid);
-            Assert.Equal("UfSigla precisa ser informada e conter exatamente 2 caracteres ex:SP", result.Errors[0].ErrorMessage);
+                Assert.Equal(expectedErrorMessage, result.Errors[0].ErrorMessage);
+            }
         }
 
-        [Fact]
-        public void DDDValidator_UfSiglaComMenosDeDoisCaracteres_DeveRetornarErro()
-        {
-            //arrange
-            var dddDto = new DDDDto()
-            {
-                DddId = 11,
-                Regiao = "São Paulo",
-                UfNome = "São Paulo",
-                UfSigla = "S"
-            };
-            //act
-            var dddValidator = new DDDValidator();
-            var result = dddValidator.Validate(dddDto);
-
-            //assert
-            Assert.False(result.IsValid);
-            Assert.Equal("UfSigla precisa ser informada e conter exatamente 2 caracteres ex:SP", result.Errors[0].ErrorMessage);
-
-
-        }
-
-        [Fact]
-        public void DDDValidator_UfSiglaComMaisDeDoisCaracteres_DeveRetornarErro()
-        {
-            //arrange
-            var dddDto = new DDDDto()
-            {
-                DddId = 11,
-                Regiao = "São Paulo",
-                UfNome = "São Paulo",
-                UfSigla = "SP1"
-            };
-
-            //act
-            var dddValidator = new DDDValidator();
-            var result = dddValidator.Validate(dddDto);
-
-            //assert
-            Assert.False(result.IsValid);
-            Assert.Equal("UfSigla precisa ser informada e conter exatamente 2 caracteres ex:SP", result.Errors[0].ErrorMessage);
-        }
-
-        [Fact]
-        public void DDDValidator_UfRegiaoInformada_DeveRetornarSucesso()
-        {
-            //arrange
-            var dddDto = new DDDDto()
-            {
-                DddId = 11,
-                Regiao = "São Paulo",
-                UfNome = "São Paulo",
-                UfSigla = "SP"
-            };
-
-            //act
-            var dddValidator = new DDDValidator();
-            var result = dddValidator.Validate(dddDto);
-
-            //assert
-            Assert.True(result.IsValid);
-        }
-
-        [Fact]
-        public void DDDValidator_UfRegiaoNaoInformada_DeveRetornarErro()
-        {
-            //arrange
-            var dddDto = new DDDDto()
-            {
-                DddId = 11,
-                Regiao = "",
-                UfNome = "São Paulo",
-                UfSigla = "SP"
-            };
-
-            //act
-            var dddValidator = new DDDValidator();
-            var result = dddValidator.Validate(dddDto);
-
-            //assert
-            Assert.False(result.IsValid);
-            Assert.Equal("UfRegiao é a principal cidade da área de abrangência, precisa ser informada, e conter no máximo 100 caracteres", result.Errors[0].ErrorMessage);
-        }
-
-        [Fact]
-        public void DDDValidator_UfRegiaoComMaisDeCemCaracteres_DeveRetornarErro()
-        {
-            //arrange
-            var dddDto = new DDDDto()
-            {
-                DddId = 11,
-                Regiao = "São Paulo, Campinas, Jundiaí, São José dos Campos, Guarulhos, Osasco, Santo André, São Bernardo do Campo",
-                UfNome = "São Paulo",
-                UfSigla = "SP"
-            };
-
-            //act
-            var dddValidator = new DDDValidator();
-            var result = dddValidator.Validate(dddDto);
-
-            //assert
-            Assert.False(result.IsValid);
-            Assert.Equal("UfRegiao é a principal cidade da área de abrangência, precisa ser informada, e conter no máximo 100 caracteres", result.Errors[0].ErrorMessage);
-        }
         [Fact]
         public void DDDDto_VerificaNome_DeveRetornarSucesso()
         {
-            //arrange
-            var dddDto = new DDDDto()
+            // Arrange
+            var dddDto = new DDDDto
             {
                 DddId = 11,
                 Regiao = "São Paulo, Campinas, Jundiaí, São José dos Campos, Guarulhos, Osasco, Santo André, São Bernardo do Campo",
@@ -163,8 +77,8 @@ namespace PosTech.Fase1.Contatos.Tests.Application
                 UfSigla = "SP"
             };
 
-            //assert
-            Assert.Equal("São Paulo",dddDto.UfNome);
+            // Assert
+            Assert.Equal("São Paulo", dddDto.UfNome);
         }
     }
 }
