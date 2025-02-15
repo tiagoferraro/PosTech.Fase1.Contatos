c0a9cfb7f8a3b95d90ae83c000ca6bd42e5ad26f
Luís Fernando Fernandes <luisfernando.fernandes@hotmail.com>
2025-02-15 17:23:52 -0300

    .

--- PosTech.Fase1.Contatos.Infra/Messaging/ContatoUpdateFila.cs
+++ PosTech.Fase1.Contatos.Infra/Messaging/ContatoUpdateFila.cs
@@ -2,8 +2,8 @@
 using Microsoft.Extensions.Configuration;
 using PosTech.Fase1.Contatos.Domain.Entities;
 using PosTech.Fase1.Contatos.Infra.Interfaces;
-using System.Text.Json.Serialization;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace PosTech.Fase1.Contatos.Infra.Messaging;
 
@@ -14,8 +14,9 @@ public class ContatoUpdateFila(
 {
     public async Task AtualizarAsync(Contato contato)
     {
+
         var rabbitMqConfig = _configuration.GetSection("RabbitMq");
-        var mensagem = JsonSerializer.Serialize(contato, new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.Always });
+        var mensagem = JsonSerializer.Serialize(contato, new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull });
 
         await _rabbitMqClient.SendMessage(mensagem, rabbitMqConfig["ExchangeUpdate"]);
     }
--- PosTech.Fase1.Contatos.Infra/Messaging/RabbitMqClient.cs
+++ PosTech.Fase1.Contatos.Infra/Messaging/RabbitMqClient.cs
@@ -1,10 +1,11 @@
-﻿using System.Text;
-using Polly.Retry;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Polly;
-using RabbitMQ.Client.Exceptions;
-using RabbitMQ.Client;
-using Microsoft.Extensions.Configuration;
+using Polly.Retry;
 using PosTech.Fase1.Contatos.Infra.Interfaces;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System.Text;
 
 namespace PosTech.Fase1.Contatos.Infra.Messaging;
 
@@ -12,10 +13,13 @@ public class RabbitMqClient : IRabbitMqClient
 {
     private readonly ConnectionFactory _connectionFactory;
     private readonly AsyncRetryPolicy _retryPolicy;
+    private readonly ILogger<RabbitMqClient> _logger;
 
-    public RabbitMqClient(IConfiguration configuration)
+    public RabbitMqClient(IConfiguration configuration, ILogger<RabbitMqClient> logger)
     {
-        var rabbitMqConfig = configuration.GetSection("RabbitMq");
+        _logger = logger; // Armazena o logger
+        var rabbitMqConfig = configuration.GetSection("RabbitMQ");
+
         _connectionFactory = new ConnectionFactory()
         {
             HostName = rabbitMqConfig["HostName"],
@@ -23,40 +27,43 @@ public class RabbitMqClient : IRabbitMqClient
             Password = rabbitMqConfig["Password"]
         };
 
-        // Configura uma política de retry com Polly
         _retryPolicy = Policy
-            .Handle<BrokerUnreachableException>() // Reexecuta em caso de falha ao conectar ao broker
-            .Or<AlreadyClosedException>()        // Ou se a conexão for encerrada prematuramente
-            .Or<Exception>()                     // Ou qualquer outra exceção
+            .Handle<BrokerUnreachableException>()
+            .Or<AlreadyClosedException>()
+            .Or<Exception>()
             .WaitAndRetryAsync(
-                retryCount: 3,                   // Número de tentativas de retry
-                sleepDurationProvider: attempt => TimeSpan.FromSeconds(2 * attempt), // Tempo entre tentativas
+                retryCount: 3,
+                sleepDurationProvider: attempt => TimeSpan.FromSeconds(2 * attempt),
                 onRetry: (exception, timespan, retryAttempt, context) =>
                 {
-                    Console.WriteLine($"Tentativa de envio {retryAttempt} falhou. Tentando novamente em {timespan.Seconds} segundos devido a: {exception.Message}");
+                    _logger.LogWarning($"Tentativa {retryAttempt} falhou. Tentando novamente em {timespan.Seconds} segundos. Erro: {exception.Message}");
                 }
             );
     }
 
     public async Task SendMessage(string message, string exchange)
     {
-        // Executa o envio com a política de retry
+        _logger.LogInformation("Iniciando envio da mensagem para RabbitMQ...");
+
         await _retryPolicy.ExecuteAsync(async () =>
         {
+            _logger.LogInformation("Tentando estabelecer conexão com RabbitMQ...");
+
             await using var connection = await _connectionFactory.CreateConnectionAsync();
+            _logger.LogInformation("Conexão com RabbitMQ estabelecida.");
+
             await using var channel = await connection.CreateChannelAsync();
+            _logger.LogInformation($"Publicando mensagem na exchange '{exchange}'.");
 
             var body = Encoding.UTF8.GetBytes(message);
 
-
-            // Publica a mensagem na fila
             await channel.BasicPublishAsync(
-                                exchange: exchange,
-                                routingKey: "",
-                                body: body);
+                exchange: exchange,
+                routingKey: "",
+                body: body
+            );
 
-            Console.WriteLine($"Mensagem enviada: {message}");
+            _logger.LogInformation($"Mensagem enviada com sucesso para '{exchange}': {message}");
         });
     }
 }
-
